afe874e0df60ca7d6cdcdca1429968ec6af632df
Kulka22 <kuzslava2211@gmail.com>
2025-06-21 16:50:27 +0300

    Console Interface
    Добавлен проект для консольного интерфейса (для отладки частей Славика)

---
+++ PasswordManager.ConsoleInterface/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using PasswordManager.Crypto;
+using PasswordManager.Data;
+using static PasswordManager.Data.DataManager.JsonManager;
+
+namespace PasswordManager.ConsoleInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
