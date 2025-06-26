0e2cbfa7c27b7ddebcf254f0fb02c61758533dd7
Kulka22 <kuzslava2211@gmail.com>
2025-06-26 16:54:37 +0300

    ConsoleInterface was Added
    Добавлен частичный функционал консольного приложения

--- PasswordManager.ConsoleInterface/Program.cs
+++ PasswordManager.ConsoleInterface/Program.cs
@@ -12,6 +12,7 @@ namespace PasswordManager.ConsoleInterface
         {
             string masterPassword;
             string filePath = "psw.json";
+            List<PasswordEntry> passwords = new List<PasswordEntry>();
 
             // Сначала проверяем первый ли у нас запуск.
             // Если первый, то просим придумать пароль.
@@ -59,6 +60,87 @@ namespace PasswordManager.ConsoleInterface
                     Console.Clear();
                 }
             }
+
+            MainProcess main = new MainProcess(masterPassword);
+            passwords = main.GetPasswords();
+
+            string inputButton;
+            do
+            {
+                Console.WriteLine("Доступные действия");
+                Console.WriteLine("1 - Вывод всех паролей.");
+                Console.WriteLine("2 - Изменить пароль.");
+                Console.WriteLine("3 - Удалить пароль.");
+                Console.WriteLine("4 - Добавить пароль.");
+                Console.WriteLine("e - Выход из программы.");
+                Console.Write("Выберите действие: ");
+                inputButton = Console.ReadLine();
+                Console.Clear();
+                switch (inputButton)
+                {
+                    case "1":
+                        {
+                            int num = 1;
+                            foreach (PasswordEntry password in passwords)
+                            {
+                                Console.Write($"{num++}. {password.Service} " +
+                                    $"{password.Login} ");
+                                Console.Write(new string('*', password.Password.Length) + ";\n");
+                            }
+                            Console.WriteLine("Сделать пароли видимыми (1 - да, 0 - нет)?");
+                            Console.Write("Ваш выбор: ");
+                            string temp = Console.ReadLine();
+                            if (temp == "1")
+                            {
+                                num = 1;
+                                foreach (PasswordEntry password in passwords)
+                                {
+                                    Console.Write($"{num++}. {password.Service} " +
+                                        $"{password.Login} {password.Password}\n");
+                                }
+                            }
+                            else if (temp != "0")
+                                Console.WriteLine("Некорректный ввод!");
+                            break;
+                        }
+                    case "2":
+                        {
+
+                            break;
+                        }
+                    case "3":
+                        {
+
+                            break;
+                        }
+                    case "4":
+                        {
+                            string service, url, login, password;
+                            Console.Write("Введите название сервиса: ");
+                            service = Console.ReadLine();
+                            Console.Write("Введите URL: ");
+                            url = Console.ReadLine();
+                            Console.Write("Введите логин: ");
+                            login = Console.ReadLine();
+                            Console.Write("Введите пароль: ");
+                            password = MainProcess.ReadPasswordConsole();
+                            main.AddPassword(new PasswordEntry() { Service = service, 
+                            Url = url, Login = login, Password = password});
+                            main.SavePasswords();
+                            break;
+                        }
+                    case "e":
+                        {
+
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey(intercept: true);
+                Console.Clear();
+            } while (true);
         }
     }
 }
